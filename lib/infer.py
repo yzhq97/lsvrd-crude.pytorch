2c1fd01646d89caad6464ef32ab72a65b4a2cefc
Zhuoqian Yang <yzhq97@gmail.com>
2019-04-02 21:31:00 +0800

    sync

--- lib/infer.py
+++ lib/infer.py
@@ -45,62 +45,63 @@ def infer(vision_model, all_ent_boxes, pred_emb, loader, writer, h5s, info, args
 
     tasks = list(all_ent_boxes.items())
     n_tasks = len(tasks)
+    similarity = PairwiseCosineSimilarity()
 
-    with torch.no_grad(): similarity = PairwiseCosineSimilarity()
+    with torch.no_grad():
 
-    loaded = []
-    loader_thread = LoaderThread(loader, tasks[0][0], loaded)
-    loader_thread.start()
-    writer_thread = None
+        loaded = []
+        loader_thread = LoaderThread(loader, tasks[0][0], loaded)
+        loader_thread.start()
+        writer_thread = None
 
-    for task_idx in trange(n_tasks):
+        for task_idx in trange(n_tasks):
 
-        image_id, ent_boxes = tasks[task_idx]
-        n_ent = len(ent_boxes)
+            image_id, ent_boxes = tasks[task_idx]
+            n_ent = len(ent_boxes)
 
-        file_idx = info[image_id]["file"]
-        array_idx = info[image_id]["idx"]
+            file_idx = info[image_id]["file"]
+            array_idx = info[image_id]["idx"]
 
-        loader_thread.join()
-        feature_map = torch.tensor(loaded[0]).float().cuda()
-        if task_idx + 1 < n_tasks:
-            loaded = []
-            loader_thread = LoaderThread(loader, tasks[task_idx+1][0], loaded)
-            loader_thread.start()
+            loader_thread.join()
+            feature_map = torch.tensor(loaded[0]).float().cuda()
+            if task_idx + 1 < n_tasks:
+                loaded = []
+                loader_thread = LoaderThread(loader, tasks[task_idx+1][0], loaded)
+                loader_thread.start()
 
-        # ent_emb = vision_model.infer_ent(feature_map, torch.tensor(ent_boxes).float().cuda())
+            # ent_emb = vision_model.infer_ent(feature_map, torch.tensor(ent_boxes).float().cuda())
 
-        sbj_boxes, obj_boxes, rel_boxes = get_triple_boxes(ent_boxes)
-        sbj_boxes = torch.tensor(sbj_boxes).float().cuda()
-        obj_boxes = torch.tensor(obj_boxes).float().cuda()
-        rel_boxes = torch.tensor(rel_boxes).float().cuda()
+            sbj_boxes, obj_boxes, rel_boxes = get_triple_boxes(ent_boxes)
+            sbj_boxes = torch.tensor(sbj_boxes).float().cuda()
+            obj_boxes = torch.tensor(obj_boxes).float().cuda()
+            rel_boxes = torch.tensor(rel_boxes).float().cuda()
 
-        n_boxes = len(rel_boxes)
-        n_batches = int(math.ceil(n_boxes / args.batch_size))
-        rel_emb = []
-        for i in range(n_batches):
-            batch_sbj = sbj_boxes[i * args.batch_size: (i + 1) * args.batch_size]
-            batch_obj = obj_boxes[i * args.batch_size: (i + 1) * args.batch_size]
-            batch_rel = rel_boxes[i * args.batch_size: (i + 1) * args.batch_size]
-            batch_rel_emb = vision_model.infer_rel(feature_map, batch_sbj, batch_obj, batch_rel)
-            rel_emb.append(batch_rel_emb)
-        rel_emb = torch.cat(rel_emb, dim=0)
+            n_boxes = len(rel_boxes)
+            n_batches = int(math.ceil(n_boxes / args.batch_size))
+            rel_emb = []
+            for i in range(n_batches):
+                batch_sbj = sbj_boxes[i * args.batch_size: (i + 1) * args.batch_size]
+                batch_obj = obj_boxes[i * args.batch_size: (i + 1) * args.batch_size]
+                batch_rel = rel_boxes[i * args.batch_size: (i + 1) * args.batch_size]
+                batch_rel_emb = vision_model.infer_rel(feature_map, batch_sbj, batch_obj, batch_rel)
+                rel_emb.append(batch_rel_emb)
+            rel_emb = torch.cat(rel_emb, dim=0)
 
-        # ent_emb_out = np.zeros([args.max_entities, cfg.vision_model.emb_dim])
-        # ent_emb_out[:n_ent, :] = ent_emb
+            # ent_emb_out = np.zeros([args.max_entities, cfg.vision_model.emb_dim])
+            # ent_emb_out[:n_ent, :] = ent_emb
 
-        s = similarity(rel_emb, pred_emb)
-        _, labels = s.max(dim=1)
-        rel_mat = labels.reshape([n_ent, n_ent])
-        rel_mat_out = -np.ones([args.max_entities, args.max_entities], dtype="int32")
-        rel_mat_out[:n_ent, :n_ent] = rel_mat
+            s = similarity(rel_emb, pred_emb)
+            _, labels = s.max(dim=1)
+            rel_mat = labels.reshape([n_ent, n_ent])
+            rel_mat_out = -np.ones([args.max_entities, args.max_entities], dtype="int32")
+            rel_mat_out[:n_ent, :n_ent] = rel_mat
 
-        rel_emb = rel_emb.reshape([n_ent, n_ent, cfg.vision_model.emb_dim])
-        rel_emb_out = np.zeros([args.max_entities, args.max_entities, cfg.vision_model.emb_dim])
-        rel_emb_out[:n_ent, :n_ent, :] = rel_emb
+            rel_emb = rel_emb.reshape([n_ent, n_ent, cfg.vision_model.emb_dim])
+            rel_emb_out = np.zeros([args.max_entities, args.max_entities, cfg.vision_model.emb_dim])
+            rel_emb_out[:n_ent, :n_ent, :] = rel_emb
 
-        if writer_thread is not None: writer_thread.join()
-        writer_thread = WriterThread(writer, image_id,
-                                     [h5s[file_idx][array_idx, :args.max_entities, :],
-                                      rel_emb_out, rel_mat])
-        writer_thread.start()
+            if writer_thread is not None: writer_thread.join()
+            writer_thread = WriterThread(writer, image_id,
+                                         [h5s[file_idx][array_idx, :args.max_entities, :],
+                                          rel_emb_out, rel_mat])
+            writer_thread.start()
