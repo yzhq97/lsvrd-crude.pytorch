d6d980675d253decc9f4ef575171a6f651769a00
Zhuoqian Yang <yzhq97@gmail.com>
2019-04-10 21:39:14 +0800

    sync

--- scripts/generate_triples.py
+++ scripts/generate_triples.py
@@ -8,7 +8,7 @@ import numpy as np
 from tqdm import tqdm, trange
 from lib.data.sym_dict import SymbolDictionary
 
-def get_entities(scene_graph, ent_dict: SymbolDictionary):
+def get_entities(scene_graph, ent_dict: SymbolDictionary, attr_dict: SymbolDictionary):
 
     ent_labels = []
     ent_boxes = []
@@ -22,9 +22,10 @@ def get_entities(scene_graph, ent_dict: SymbolDictionary):
         idx2eid.append(eid)
         ent_labels.append(ent_dict.sym2idx[entity["name"]])
         ent_boxes.append((entity["x"], entity["y"], entity["x"]+entity["w"], entity["y"]+entity["h"]))
-        ent_attrs.append(entity["attributes"])
+        attributes = [attr_dict.sym2idx[attr] for attr in entity["attributes"]]
+        ent_attrs.append(attributes)
 
-    return ent_labels, ent_boxes, eid2idx, idx2eid
+    return ent_labels, ent_attrs, ent_boxes, eid2idx, idx2eid
 
 def get_rel_mat(eid2idx, scene_graph, pred_dict: SymbolDictionary):
 
@@ -44,7 +45,7 @@ def get_rel_mat(eid2idx, scene_graph, pred_dict: SymbolDictionary):
 
     return rel_mat
 
-def get_triples_from_gt(gt_boxes, ent_labels, rel_mat, pred_use_prob, use_none_label):
+def get_triples_from_gt(gt_boxes, ent_labels, ent_attrs, rel_mat, pred_use_prob, use_none_label):
 
     entries = []
 
@@ -52,26 +53,20 @@ def get_triples_from_gt(gt_boxes, ent_labels, rel_mat, pred_use_prob, use_none_l
     for i in range(n_ent):
         for j in range(n_ent):
             if i != j:
-                if use_none_label and len(rel_mat[i][j]) == 0:
-                    if random.random() > pred_use_prob[0]: continue
-                    entry = {
-                        "sbj_box": gt_boxes[i],
-                        "sbj_label": ent_labels[i],
-                        "obj_box": gt_boxes[j],
-                        "obj_label": ent_labels[j],
-                        "pred_label": 0
-                    }
+                entry = {
+                    "sbj_box": gt_boxes[i],
+                    "obj_box": gt_boxes[j],
+                    "sbj_label": ent_labels[i],
+                    "obj_label": ent_labels[j],
+                    "sbj_attrs": ent_attrs[i],
+                    "obj_attrs": ent_attrs[j],
+                    "pred_label": 0
+                }
+                if use_none_label and len(rel_mat[i][j]) == 0 and random.random() <= pred_use_prob[0]:
                     entries.append(entry)
                 for pred_id in rel_mat[i][j]:
-                    if random.random() > pred_use_prob[pred_id]: continue
-                    entry = {
-                        "sbj_box": gt_boxes[i],
-                        "sbj_label": ent_labels[i],
-                        "obj_box": gt_boxes[j],
-                        "obj_label": ent_labels[j],
-                        "pred_label": pred_id
-                    }
-                    entries.append(entry)
+                    if random.random() <= pred_use_prob[pred_id]:
+                        entries.append(entry)
 
     return entries
 
@@ -114,7 +109,7 @@ def compute_iou_mat(a, b):
     return iou_mat
 
 
-def get_triples_from_proposals(proposals, gt_boxes, ent_labels, rel_mat, pred_use_prob, use_none_label, iou_thresh):
+def get_triples_from_proposals(proposals, gt_boxes, ent_labels, ent_attrs, rel_mat, pred_use_prob, use_none_label, iou_thresh):
 
     entries = []
 
@@ -128,25 +123,20 @@ def get_triples_from_proposals(proposals, gt_boxes, ent_labels, rel_mat, pred_us
 
         for j in range(len(gt_boxes)):
             if i_gt != j:
+                entry = {
+                    "sbj_box": proposals[i],
+                    "obj_box": gt_boxes[j],
+                    "sbj_label": sbj_label,
+                    "obj_label": ent_labels[j],
+                    "sbj_attrs": ent_attrs[i_gt],
+                    "obj_attrs": ent_attrs[j],
+                    "pred_label": 0
+                }
                 if use_none_label and (len(rel_mat[i][j]) == 0 or sbj_label == 0):
                     if sbj_label != 0 and random.random() > pred_use_prob[0]: continue
-                    entry = {
-                        "sbj_box": proposals[i],
-                        "sbj_label": sbj_label,
-                        "obj_box": gt_boxes[j],
-                        "obj_label": ent_labels[j],
-                        "pred_label": 0
-                    }
                     entries.append(entry)
                 for pred_id in rel_mat[i][j]:
                     if random.random() > pred_use_prob[pred_id]: continue
-                    entry = {
-                        "sbj_box":proposals[i],
-                        "sbj_label": sbj_label,
-                        "obj_box": gt_boxes[j],
-                        "obj_label": ent_labels[j],
-                        "pred_label": pred_id
-                    }
                     entries.append(entry)
 
         if sbj_label == 0:
@@ -155,9 +145,11 @@ def get_triples_from_proposals(proposals, gt_boxes, ent_labels, rel_mat, pred_us
                     if random.random() > pred_use_prob[0]: continue
                     entry = {
                         "sbj_box": proposals[i],
-                        "sbj_label": 0,
                         "obj_box": proposals[j],
+                        "sbj_label": 0,
                         "obj_label": 0,
+                        "sbj_attrs": [],
+                        "obj_attrs": [],
                         "pred_label": 0
                     }
                     entries.append(entry)
@@ -176,6 +168,7 @@ if __name__ == "__main__":
     n_rel_max = 100000
 
     ent_dict_path = "cache/ent_dict.json"
+    attr_dict_path = "cache/attr_dict.json"
     pred_dict_path = "cache/pred_dict_%d.json" % n_preds
     pred_use_prob_path = "cache/pred_use_prob_%d_max_%d.json" % (n_preds, n_rel_max)
     balanced = False
@@ -193,6 +186,7 @@ if __name__ == "__main__":
 
     # load dictionaries
     ent_dict = SymbolDictionary.load_from_file(ent_dict_path)
+    attr_dict = SymbolDictionary.load_from_file(attr_dict_path)
     pred_dict = SymbolDictionary.load_from_file(pred_dict_path)
     pred_use_prob = json.load(open(pred_use_prob_path))
 
@@ -213,12 +207,12 @@ if __name__ == "__main__":
         for image_id, scene_graph in tqdm(scene_graphs.items()):
 
             meta = gqa_objects_info[image_id]
-            ent_labels, ent_boxes, eid2idx, idx2eid = get_entities(scene_graph, ent_dict)
+            ent_labels, ent_attrs, ent_boxes, eid2idx, idx2eid = get_entities(scene_graph, ent_dict, attr_dict)
             rel_mat = get_rel_mat(eid2idx, scene_graph, pred_dict)
 
             graph_entries = []
 
-            gt_entries = get_triples_from_gt(ent_boxes, ent_labels, rel_mat, pred_use_prob, use_none_label)
+            gt_entries = get_triples_from_gt(ent_boxes, ent_labels, ent_attrs, rel_mat, pred_use_prob, use_none_label)
             for entry in gt_entries:
                 entry["image_id"] = image_id
                 entry["width"] = meta["width"]
@@ -230,7 +224,7 @@ if __name__ == "__main__":
                 file_idx = meta["file"]
                 idx = meta["idx"]
                 proposals = h5_boxes[file_idx][idx, :n_proposal_boxes, :]
-                proposal_entries = get_triples_from_proposals(proposals, ent_boxes, ent_labels, rel_mat, pred_use_prob, use_none_label, iou_thresh)
+                proposal_entries = get_triples_from_proposals(proposals, ent_boxes, ent_attrs, ent_labels, rel_mat, pred_use_prob, use_none_label, iou_thresh)
                 graph_entries.extend(proposal_entries)
 
             entries.extend(graph_entries)
