ce026717ebe55d6283d666fd56573c1a8fb8ccf3
Zhuoqian Yang <yzhq97@gmail.com>
2019-04-10 20:24:58 +0800

    sync

--- scripts/build_dicts.py
+++ scripts/build_dicts.py
@@ -36,14 +36,20 @@ if __name__ == "__main__":
 
     word_dict = SymbolDictionary()
     word_dict.add_sym("unknown")
-    len_cnt = [0] * 8
 
     ent_dict = SymbolDictionary()
-    pred_dict = SymbolDictionary()
     ent_dict.add_sym("unknown")
-    pred_dict.add_sym("unknown")
     ent_cnt = {}
+    ent_len_cnt = [0] * 8
+
+    pred_dict = SymbolDictionary()
+    pred_dict.add_sym("unknown")
     pred_cnt = {}
+    pred_len_cnt = [0] * 8
+
+    attr_dict = SymbolDictionary()
+    attr_cnt = {}
+    attr_len_cnt = [0] * 8
 
     print("    running statistics ...")
 
@@ -63,10 +69,15 @@ if __name__ == "__main__":
                 else:
                     ent_cnt[ent["name"]] = 1
 
+                attrs = ent["attributes"]
+                for attr in attrs:
+                    if attr in attr_cnt:
+                        attr_cnt[attr] += 1
+                    else:
+                        attr_cnt[attr] = 1
+
                 rels = ent["relations"]
-
                 for rel in rels:
-
                     if rel["name"] in pred_cnt:
                         pred_cnt[rel["name"]] += 1
                     else:
@@ -77,15 +88,21 @@ if __name__ == "__main__":
     for ent_name, cnt in tqdm(ent_cnt.items()):
         ent_dict.add_sym(ent_name)
         tokens = word_dict.tokenize(ent_name, add_sym=True)
-        len_cnt[len(tokens)] += 1
+        ent_len_cnt[len(tokens)] += 1
+
+    for attr_name, cnt in tqdm(attr_cnt.items()):
+        attr_dict.add_sym(attr_name)
+        tokens = word_dict.tokenize(attr_name, add_sym=True)
+        attr_len_cnt[len(tokens)] += 1
 
     for pred_name, cnt in tqdm(pred_cnt.items()):
         pred_dict.add_sym(pred_name)
         tokens = word_dict.tokenize(pred_name, add_sym=True)
-        len_cnt[len(tokens)] += 1
+        pred_len_cnt[len(tokens)] += 1
 
     word_dict.dump_to_file("cache/word_dict.json")
     ent_dict.dump_to_file("cache/ent_dict.json")
+    attr_dict.dump_to_file("cache/attr_dict.json")
     pred_dict.dump_to_file("cache/pred_dict.json")
 
     # get glove words
@@ -107,7 +124,7 @@ if __name__ == "__main__":
     np.save("cache/word_emb_init_%dd.npy" % emb_dim, word_emb)
 
     print()
-    print("    %d entity categories, %d predicates" % (len(ent_dict), len(pred_dict)))
+    print("    %d entity categories, %d attributes, %d predicates" % (len(ent_dict), len(attr_dict), len(pred_dict)))
 
     print()
     print("    %d words in word_dict" % len(word_dict))
@@ -115,6 +132,16 @@ if __name__ == "__main__":
     print(uninit)
 
     print()
-    print("    token length count (by category):")
-    for length, number in enumerate(len_cnt):
+    print("    ent token length count (by category):")
+    for length, number in enumerate(ent_len_cnt):
+        print("%2d %d" % (length, number))
+
+    print()
+    print("    attr token length count (by category):")
+    for length, number in enumerate(attr_len_cnt):
+        print("%2d %d" % (length, number))
+
+    print()
+    print("    pred token length count (by category):")
+    for length, number in enumerate(pred_len_cnt):
         print("%2d %d" % (length, number))
--- scripts/generate_triples.py
+++ scripts/generate_triples.py
@@ -12,6 +12,7 @@ def get_entities(scene_graph, ent_dict: SymbolDictionary):
 
     ent_labels = []
     ent_boxes = []
+    ent_attrs = []
     eid2idx = {}
     idx2eid = []
 
@@ -21,6 +22,7 @@ def get_entities(scene_graph, ent_dict: SymbolDictionary):
         idx2eid.append(eid)
         ent_labels.append(ent_dict.sym2idx[entity["name"]])
         ent_boxes.append((entity["x"], entity["y"], entity["x"]+entity["w"], entity["y"]+entity["h"]))
+        ent_attrs.append(entity["attributes"])
 
     return ent_labels, ent_boxes, eid2idx, idx2eid
 
@@ -42,7 +44,7 @@ def get_rel_mat(eid2idx, scene_graph, pred_dict: SymbolDictionary):
 
     return rel_mat
 
-def get_roidb_from_gt(gt_boxes, ent_labels, rel_mat, pred_use_prob, use_none_label):
+def get_triples_from_gt(gt_boxes, ent_labels, rel_mat, pred_use_prob, use_none_label):
 
     entries = []
 
@@ -73,12 +75,102 @@ def get_roidb_from_gt(gt_boxes, ent_labels, rel_mat, pred_use_prob, use_none_lab
 
     return entries
 
+def compute_iou(a, b):
+
+    x1a, x2a, y1a, y2a = a[:, 0], a[:, 1], a[:, 2], a[:, 3]
+    x1b, x2b, y1b, y2b = b[:, 0], b[:, 1], b[:, 2], b[:, 3]
+
+    # intersection
+    x1 = np.maximum(x1a, x1b)
+    x2 = np.minimum(x2a, x2b)
+    y1 = np.maximum(y1a, y1b)
+    y2 = np.maximum(y2a, y2b)
+    inter_width = x2 - x1
+    inter_height = y2 - y1
+    inter_width[inter_width < 0] = 0
+    inter_height[inter_height < 0] = 0
+    intersection = inter_height * inter_width
+
+    # union
+    a_area = (x2a - x1a) * (y2a - y1a)
+    b_area = (x2b - x1b) * (y2b - y1b)
+    union = a_area + b_area - intersection
+
+    return intersection / union
+
+def compute_iou_mat(a, b):
+
+    a = np.array(a)
+    b = np.array(b)
+    a_exp = np.expand_dims(a, axis=1)
+    b_exp = np.expand_dims(b, axis=0)
+    a_exp = np.repeat(a, len(b), axis=1).reshape([-1, 4])
+    b_exp = np.repeat(b, len(a), axis=0).reshape([-1, 4])
+
+    ious = compute_iou(a_exp, b_exp)
+
+    iou_mat = ious.reshape([len(a), len(b)])
+
+    return iou_mat
+
+
+def get_triples_from_proposals(proposals, gt_boxes, ent_labels, rel_mat, pred_use_prob, use_none_label, iou_thresh):
+
+    entries = []
+
+    iou_mat = compute_iou(proposals, gt_boxes)
+    max_iou = np.max(iou_mat, axis=1)
+    argmax_iou = np.argmax(iou_mat, axis=1)
+
+    for i in range(len(proposals)):
+        i_gt = argmax_iou[i]
+        sbj_label = ent_labels[i_gt] if max_iou[i] >= iou_thresh else 0
+
+        for j in range(len(gt_boxes)):
+            if i_gt != j:
+                if use_none_label and (len(rel_mat[i][j]) == 0 or sbj_label == 0):
+                    if sbj_label != 0 and random.random() > pred_use_prob[0]: continue
+                    entry = {
+                        "sbj_box": proposals[i],
+                        "sbj_label": sbj_label,
+                        "obj_box": gt_boxes[j],
+                        "obj_label": ent_labels[j],
+                        "pred_label": 0
+                    }
+                    entries.append(entry)
+                for pred_id in rel_mat[i][j]:
+                    if random.random() > pred_use_prob[pred_id]: continue
+                    entry = {
+                        "sbj_box":proposals[i],
+                        "sbj_label": sbj_label,
+                        "obj_box": gt_boxes[j],
+                        "obj_label": ent_labels[j],
+                        "pred_label": pred_id
+                    }
+                    entries.append(entry)
+
+        if sbj_label == 0:
+            for j in range(proposals):
+                if i != j and max_iou[j] < iou_thresh:
+                    if random.random() > pred_use_prob[0]: continue
+                    entry = {
+                        "sbj_box": proposals[i],
+                        "sbj_label": 0,
+                        "obj_box": proposals[j],
+                        "obj_label": 0,
+                        "pred_label": 0
+                    }
+                    entries.append(entry)
+
+    return entries
+
 if __name__ == "__main__":
 
     print("generating triples ...")
 
     image_dir = "data/gqa/images"
     box_source = "gt"
+    iou_thresh = 0.75
     out_dir = "cache"
     n_preds = 311
     n_rel_max = 100000
@@ -107,7 +199,7 @@ if __name__ == "__main__":
     # load h5s
     h5_paths = [ os.path.join(object_features_dir, "gqa_objects_%d.h5" % i) for i in range(n_h5s)]
     h5_boxes = [ h5py.File(path, "r").get("bboxes") for path in h5_paths ]
-    h5_lookup = json.load(open(os.path.join(object_features_dir, "gqa_objects_info.json")))
+    gqa_objects_info = json.load(open(os.path.join(object_features_dir, "gqa_objects_info.json")))
 
     # start
 
@@ -120,21 +212,26 @@ if __name__ == "__main__":
 
         for image_id, scene_graph in tqdm(scene_graphs.items()):
 
-            meta = h5_lookup[image_id]
+            meta = gqa_objects_info[image_id]
             ent_labels, ent_boxes, eid2idx, idx2eid = get_entities(scene_graph, ent_dict)
             rel_mat = get_rel_mat(eid2idx, scene_graph, pred_dict)
 
-            if box_source == "gt":
-                graph_entries = get_roidb_from_gt(ent_boxes, ent_labels, rel_mat, pred_use_prob, use_none_label)
-                for entry in graph_entries:
-                    entry["image_id"] = image_id
-                    entry["width"] = meta["width"]
-                    entry["height"] = meta["height"]
-            # elif box_source == "proposal":
-            #     proposals = h5_boxes[meta["file"]][meta["idx"], :meta["objectsNum"], :]
-            #     sbj_boxes, obj_boxes, rlp_labels = get_roidb_from_proposals(proposals, ent_boxes, ent_labels, rel_mat)
-            else:
-                raise Exception("invalid box source")
+            graph_entries = []
+
+            gt_entries = get_triples_from_gt(ent_boxes, ent_labels, rel_mat, pred_use_prob, use_none_label)
+            for entry in gt_entries:
+                entry["image_id"] = image_id
+                entry["width"] = meta["width"]
+                entry["height"] = meta["height"]
+                graph_entries.append(gt_entries)
+
+            if box_source == "gt+proposals":
+                n_proposal_boxes = meta["objectsNum"]
+                file_idx = meta["file"]
+                idx = meta["idx"]
+                proposals = h5_boxes[file_idx][idx, :n_proposal_boxes, :]
+                proposal_entries = get_triples_from_proposals(proposals, ent_boxes, ent_labels, rel_mat, pred_use_prob, use_none_label, iou_thresh)
+                graph_entries.extend(proposal_entries)
 
             entries.extend(graph_entries)
             triple_cnt[split] += len(graph_entries)
@@ -143,7 +240,8 @@ if __name__ == "__main__":
         #     json.dump(entries, f)
 
         out_name = split + "_triples"
-        out_name = out_name + "_%s_boxes" % box_source
+        out_name = out_name + "_%s" % box_source
+        if box_source == "gt+proposals": out_name = out_name + "_%.f" % iou_thresh
         if use_none_label: out_name = out_name + "_use_none"
         out_name = out_name + "_%d_max_%d" % (n_preds, n_rel_max)
         out_name = out_name + ".pkl"
