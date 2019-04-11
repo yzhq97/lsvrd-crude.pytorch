c65ac616a0d9059a1ea69da687bed61b24ca3367
Zhuoqian Yang <yzhq97@gmail.com>
2019-04-11 15:02:47 +0800

    sync

--- lib/data/dataset.py
+++ lib/data/dataset.py
@@ -177,13 +177,11 @@ class GQATriplesDataset(Dataset):
                 sbj_text = self.ent_dict.idx2sym[entry.sbj_label]
                 for i in range(min(self.n_attrs, len(entry.sbj_attrs))):
                     sbj_text = self.attr_dict.idx2sym[entry.sbj_attrs[i]] + " " + sbj_text
-                print(sbj_text)
                 entry.sbj_tokens, entry.sbj_seq_len = self.tokenize(sbj_text, keep_tail=True)
 
                 obj_text = self.ent_dict.idx2sym[entry.obj_label]
                 for i in range(min(self.n_attrs, len(entry.obj_attrs))):
                     obj_text = self.attr_dict.idx2sym[entry.obj_attrs[i]] + " " + obj_text
-                print(obj_text)
                 entry.obj_tokens, entry.obj_seq_len = self.tokenize(obj_text, keep_tail=True)
 
                 pred_text = self.pred_dict.idx2sym[entry.pred_label]
